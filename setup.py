258a8ab57d3133765e53359b5f37cb9317620207
Nick Nelson <nnelson@thestranger.com>
2020-01-20 12:37:59 -0800

    fixing setup, adding license

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@ import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='django-related-models',
     version='0.1',
+    license="BSD",
     packages=['django_related_models'],
     description='A related model sidebar for Django Admin',
-    long_description=README,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Nick Nelson',
-    author_email='whoisnicknelson@gmail.com',
+    author_email='nick.eugene.nelson@gmail.com',
     url='https://github.com/nicknelson/django-related-models/',
-    license='MIT',
     install_requires=[
         'Django>=2.0',
     ]
